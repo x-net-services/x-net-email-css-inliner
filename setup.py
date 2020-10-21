6204df3b1fb632084b9386e529b06d16e9b002ee
Michael Hacker <mh@x-net.at>
2020-10-21 10:40:24 +0200

    Update MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import (
-    config,
     find_packages,
     setup,
 )
 
 import x_net_email_css_inliner
 
-
-conf = config.read_configuration("setup.cfg")
-metadata = conf.get("metadata")
 
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
@@ -17,15 +13,17 @@ setup(
     version=x_net_email_css_inliner.__version__,
     long_description=long_description,
     long_description_content_type="text/markdown",
+    include_package_data=True,
     packages=find_packages(),
+    python_requires=">=3.6",
     install_requires=[
         "beautifulsoup4==4.9.3",
         "requests==2.24.0",
         "tinycss2==1.0.2",
     ],
     classifiers=[
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.6",
         "Topic :: Internet :: WWW/HTTP",
     ],
-    **metadata
 )
